d58b40c46161818e45f1bea4cc8706c2adcf466d
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2022-08-14 15:52:05 -0400

    Scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,34 @@
 const app = Vue.createApp({
   data: function () {
     return {
+      language: 'en-us',
       trophies: window.trophies
     };
   },
   methods: {
     styling: function (trophyId) {
+      const { width, height, size, trophiesPerRow } = this.imageSizes;
+      const xOffset = width * (trophyId % trophiesPerRow) * -1;
+      const yOffset = height * Math.floor(trophyId / trophiesPerRow) * -1;
+      return [
+        'width: ' + width + 'px',
+        'height: ' + height + 'px',
+        'background-size: ' + size + 'px',
+      	'background-position: ' + xOffset + 'px ' + yOffset + 'px'
+      ].join(';');
+    },
+    scroll: function () {
+      const id = window.location.hash.replace('#', '');
+      if (id) {
+        document.getElementById(id).scrollIntoView();
+      }
+    },
+    sizeTh: function (size) {
+      return Math.round((size / 1903) * 100) + '%';
+    }
+  },
+  computed: {
+    imageSizes: function () {
       const originalSpriteWidth = 128;
       const originalSpriteHeight = 144;
       const trophiesPerRow = 12;
@@ -16,15 +39,17 @@ const app = Vue.createApp({
       const height = originalSpriteHeight * reductionRatio;
       const size = totalSpriteSheetWidth * reductionRatio;
 
-      const xOffset = width * (trophyId % trophiesPerRow) * -1;
-      const yOffset = height * Math.floor(trophyId / trophiesPerRow) * -1;
-
-      return [
-        'width: ' + width + 'px',
-        'height: ' + height + 'px',
-        'background-size: ' + size + 'px',
-      	'background-position: ' + xOffset + 'px ' + yOffset + 'px'
-      ].join(';');
+      return {
+        width,
+        height,
+        size,
+        trophiesPerRow
+      };
     }
+  },
+  created: function () {
+    setTimeout(() => {
+      this.scroll();
+    }, 350);
   }
 }).mount('#app');
