705626f9056a1a4186ee048a06d9722c74fd4057
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2022-08-26 08:39:41 -0400

    Language selection styling

--- components/side-bar.vue
+++ components/side-bar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="sidebar">
     <div class="controls">
-      <h1>"{{ dictionary[language].allTrophies }}"</h1>
-      <h2>{{ dictionary[language].speedrunTracker }}</h2>
+      <h1 :class="{ jp: isJP }">"{{ dictionary[language].allTrophies }}"</h1>
+      <h2 :class="{ jp: isJP }">{{ dictionary[language].speedrunTracker }}</h2>
 
       <div>
         <button
@@ -76,23 +76,68 @@
       </div>
 
       <div class="form-control">
-        <label><strong>{{ dictionary[language].language }}</strong></label>
-        <span
-          v-text="'🇺🇸'"
-          class="cursor flag"
-          :class="{ selected: language === 'en' }"
-          title="EN-US"
-          @click="setLanguage('en')"
-        ></span>
-        <span
-          v-text="'🇯🇵'"
-          class="cursor flag"
-          :class="{ selected: isJP }"
-          title="JP"
-          @click="setLanguage('jp')"
-        ></span>
+        <label
+          class="language-label"
+          :class="{ jp: isJP }"
+        >
+          <strong>{{ dictionary[language].language }}</strong>
+        </label>
+
+        <div class="language-controls">
+          <button
+            class="flag-language-container jp"
+            :class="{ active: isJP }"
+            title="JP"
+            @click="setLanguage('jp')"
+          >
+            <div class="flag-container jp">
+              <svg class="flag jp" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600">
+                <rect fill="#FFF" height="600" width="900"/>
+                <circle fill="#BC002D" cx="450" cy="300" r="180"/>
+              </svg>
+            </div>
+            <div class="language-name jp">日本語</div>
+          </button>
+          <button
+            class="flag-language-container en"
+            :class="{ active: isEN }"
+            title="EN-US"
+            @click="setLanguage('en')"
+          >
+            <div class="flag-container en">
+              <svg class="flag en" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 7410 3900">
+                <rect width="7410" height="3900" fill="#B22234"/>
+                <path d="M0,450H7410m0,600H0m0,600H7410m0,600H0m0,600H7410m0,600H0" stroke="#FFF" stroke-width="300"/>
+                <rect width="2964" height="2100" fill="#3C3B6E"/>
+                <g fill="#FFF">
+                  <g id="s18">
+                    <g id="s9">
+                      <g id="s5">
+                        <g id="s4">
+                          <path id="s" d="M247,90 317.534230,307.082039 132.873218,172.917961H361.126782L176.465770,307.082039z"/>
+                          <use xlink:href="#s" y="420"/>
+                          <use xlink:href="#s" y="840"/>
+                          <use xlink:href="#s" y="1260"/>
+                        </g>
+                        <use xlink:href="#s" y="1680"/>
+                      </g>
+                      <use xlink:href="#s4" x="247" y="210"/>
+                    </g>
+                    <use xlink:href="#s9" x="494"/>
+                  </g>
+                  <use xlink:href="#s18" x="988"/>
+                  <use xlink:href="#s9" x="1976"/>
+                  <use xlink:href="#s5" x="2470"/>
+                </g>
+              </svg>
+            </div>
+            <div class="language-name en">English</div>
+          </button>
+        </div>
       </div>
+
     </div>
+
 
     <div class="logo-container">
       <img
@@ -155,6 +200,9 @@ export default {
     },
     isTrophyView: function () {
       return ['trophy', 'grid'].includes(this.view);
+    },
+    isEN: function () {
+      return this.language === 'en';
     },
     ...Pinia.mapState(store, [
       'dictionary',
